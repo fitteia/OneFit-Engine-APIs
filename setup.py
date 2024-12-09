c90b39c1599dd27b06a47513bd8ce545898ce085
Pedro Sebastiao <pjsebastiao@gmail.com>
2024-12-09 12:58:06 +0000

    0.02.03

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ofe",
-    version="0.01.0",
+    version="0.02.03",
     description="A Python module to use OFE.",
     packages=find_packages(),  
     install_requires=["requests"],
