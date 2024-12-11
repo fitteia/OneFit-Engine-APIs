b96cfc6b9f7124e45fc1b39453bb14365a342e8e
Pedro Sebastiao <pjsebastiao@gmail.com>
2024-12-11 14:32:55 +0000

    0.9.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ofeapi",
-    version="0.9.4",
+    version="0.9.5",
     description="A Pyhton module CLI API to fit data using OneFit-Engine (ofe)",
     packages=find_packages(),  
     install_requires=["requests"],
