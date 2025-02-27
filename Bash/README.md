ace618bb37ece8290af750547ee5dce23e5f6cf0
Pedro Sebastiao <pjsebastiao@gmail.com>
2025-02-27 12:05:46 +0100

    0.1.3

--- Bash/README.md
+++ Bash/README.md
@@ -8,3 +8,4 @@ host-prompt> source ofe-named-options.sh
 
 To fit a data file with an exponential
 host-prompt> fit-exp-logx <datafile>  
+
--- Bash/ofe.sh
+++ Bash/ofe.sh
@@ -1,13 +1,38 @@
-#!/usr/bin/env sh
+# examples for functions to help repeating fit procedures
 
 fit () { 
+	if [ -n "$3" ]; then 
+		curl -F "file=@$1" -F "function=$2" -F "logx=no" -F "autox=no" -F "autoy=no"  -F "download=$3" http://192.92.147.107:8142/fit
+	else
+		curl -F "file=@$1" -F "function=$2" -F "logx=no" -F "autox=no" -F "autoy=no"  http://192.92.147.107:8142/fit
+	fi
+}
+
+fit-logx () { 
+	if [ -n "$3" ]; then 
+		curl -F "file=@$1" -F "function=$2" -F "logx=yes" -F "autox=no" -F "autoy=no"  -F "download=$3" http://192.92.147.107:8142/fit
+	else
+		curl -F "file=@$1" -F "function=$2" -F "logx=yes" -F "autox=no" -F "autoy=no"  http://192.92.147.107:8142/fit
+	fi
+}
+ 
+fit-exp-logx () { 
+	if [ -n "$2" ]; then 
+		curl -F "file=@$1" -F "function=a: one exponential" -F "logx=yes" -F "autox=no" -F "autoy=no"  -F "download=$2" http://192.92.147.107:8142/fit 
+	else 
+		curl -F "file=@$1" -F "function=a: one exponential" -F "logx=yes" -F "autox=no" -F "autoy=no"  http://192.92.147.107:8142/fit 
+	fi
+}
+
+afit () { 
 	if [ -n "$3" ]; then 
 		curl -F "file=@$1" -F "function=$2" -F "logx=no" -F "autox=yes" -F "autoy=yes"  -F "download=$3" http://192.92.147.107:8142/fit
 	else
 		curl -F "file=@$1" -F "function=$2" -F "logx=no" -F "autox=yes" -F "autoy=yes"  http://192.92.147.107:8142/fit
 	fi
 }
-fit-logx () { 
+
+afit-logx () { 
 	if [ -n "$3" ]; then 
 		curl -F "file=@$1" -F "function=$2" -F "logx=yes" -F "autox=yes" -F "autoy=yes"  -F "download=$3" http://192.92.147.107:8142/fit
 	else
@@ -15,12 +40,11 @@ fit-logx () {
 	fi
 }
  
-fit-exp-logx () { 
+afit-exp-logx () { 
 	if [ -n "$2" ]; then 
 		curl -F "file=@$1" -F "function=a: one exponential" -F "logx=yes" -F "autox=yes" -F "autoy=yes"  -F "download=$2" http://192.92.147.107:8142/fit 
 	else 
 		curl -F "file=@$1" -F "function=a: one exponential" -F "logx=yes" -F "autox=yes" -F "autoy=yes"  http://192.92.147.107:8142/fit 
 	fi
 }
-
 
