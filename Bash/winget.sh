ac14be8687f43e2d6963fc034478346d20ac5053
Pedro Sebastiao <pjsebastiao@gmail.com>
2025-06-10 12:27:44 +0200

    0.1.11

---
+++ Bash/winget.sh
@@ -0,0 +1,9 @@
+# https://tinyurl.com/26bmxpfj
+#
+winget install -e --id Git.Git
+winget install -e --id=jqlang.jq 
+winget install -e --id=cURL.cURL
+winget install -e --id 7zip.7zip
+winget install -e --id=gnuplot.gnuplot
+
+git clone https://github.com/fitteia/OneFit-Engine-APIs.git
