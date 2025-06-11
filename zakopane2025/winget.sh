9ffde39e754a83761732044898906c0a7dbc87e9
Pedro Sebastiao <pjsebastiao@gmail.com>
2025-06-11 12:46:56 +0200

    0.1.11

--- zakopane2025/winget.sh
+++ zakopane2025/winget.sh
@@ -1,4 +1,5 @@
-# https://tinyurl.com/26bmxpfj
+#
+# For windows hosts
 #
 winget install -e --id Git.Git
 winget install -e --id jqlang.jq 
@@ -9,4 +10,17 @@ winget install -e --id Oracle.VirtualBox
 
 https://www.debian.org/distrib/netinst (download amd64 or amr64 or ...)
 
+
+#
+# In the Host ou Guest systems
+#
+
 git clone https://github.com/fitteia/OneFit-Engine-APIs.git
+
+curl http://192.92.147.107:8142/fit/gul -F "file=@p96m.sav" -F "download=zip" -F "logx=yes" --silent --output OFE.zip
+
+bash ../nmrd.sh gul p96p.sef
+
+bash ../sdf.sh gul c12_60.sdf c:0.6 dum/1e7
+
+bash ../stelar-sef.sh gul p96 c:0.6 dum/1e7
